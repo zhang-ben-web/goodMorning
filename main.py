998793d8375b49f2ebe549daa695a2e5c3671fc6
zhang-ben-web <79961628+zhang-ben-web@users.noreply.github.com>
2023-10-20 14:49:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,11 +57,11 @@ def get_random_color():
 
 
 client = WeChatClient(app_id, app_secret)
-current_date = datetime.now().date()
+current_date = datetime。datetime.now()
 wm = WeChatMessage(client)
 wea, temperature, min_temperature = get_weather()
 data = {
-  "date": {"value": current_date, "color":get_random_color()},
+  "date": {"value": current_date.year, "color":get_random_color()},
   "city": {"value": city, "color":get_random_color()},
   "weather":{"value":wea, "color":get_random_color()},
   "temperature":{"value":temperature, "color":get_random_color()},
