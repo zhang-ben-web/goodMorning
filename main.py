df85e738c6b07d306d2a182c118bdcd535be4417
zhang-ben-web <79961628+zhang-ben-web@users.noreply.github.com>
2023-10-30 11:07:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,10 +69,10 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature, min_temperature, current_date = get_weather()
-text_color = "位置：临汾" + "\n" + "天气：" + {{wea}}+ "\n" + "温度：" + {{min_temperature}} + "℃ ~" + {{temperature}} + "℃" + "\n" + "今天是我们相识的第"+{{get_count()}}+ "天" + "\n" + "距离你的生日还有" + {{get_birthday()}}+ "天" + "\n" + {{get_dujitang()}}+ "\n" + {{get_words()}}+ "\n" +{{get_pengyouquan()}}
+text_color = "位置：临汾" + "\n" + "天气：" + {wea}+ "\n" + "温度：" + {min_temperature} + "℃ ~" + {temperature} + "℃" + "\n" + "今天是我们相识的第"+{get_count()}+ "天" + "\n" + "距离你的生日还有" + {get_birthday()}+ "天" + "\n" + {get_dujitang()}+ "\n" + {get_words()}+ "\n" +{get_pengyouquan()}
 data = {
   "date": {"value": current_date, "color": random_color},
-  "text_color" : {"value": text_color},
+  "text_color" : {"value": text_color, "color": random_color},
   "city": {"value": city, "color": random_color},
   "weather":{"value":wea, "color": generate_random_color()},
   "temperature":{"value":temperature, "color": "#fc5531"},
